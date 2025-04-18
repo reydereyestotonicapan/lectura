7e9d11e2ede4ac8c77c93d41b54c78018b7d10d6
Miguel Menchu <mmenchu@compassion.com>
2025-04-18 02:21:50 -0600

    feat: add Filament Shield for user role and permission management verifyed

---
+++ database/factories/RoleFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Spatie\Permission\Models\Role;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class RoleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Role::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'super_admin',
+            'guard_name' => 'web',
+        ];
+    }
+}

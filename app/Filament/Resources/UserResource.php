bd23974aa50715752d7e9ea263c5b60a7654fda6
Miguel Menchu <mmenchu@compassion.com>
2025-04-30 23:11:56 -0600

    actualizar usuario veriricado

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -31,10 +31,6 @@ class UserResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required()
-                    ->maxLength(255),
                 Forms\Components\Select::make('roles')
                     ->relationship('roles', 'name')
                     ->multiple()

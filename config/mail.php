ca7caaa88111db79f5beb43decfb295cbcb8d46c
Miguel Menchu <mmenchu@compassion.com>
2025-05-20 00:47:21 -0600

    Send email for reset password

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => env('SES_MAIL_MAILER', 'log'),
 
     /*
     |--------------------------------------------------------------------------
